f450d0a5e332ecf6567811622c6a76bec05625eb
kk13 <kkbsitehost@gmail.com>
2023-01-02 21:05:45 +0800

    Update header.js

--- header.js
+++ header.js
@@ -6,8 +6,7 @@ document.getElementById("mycss").innerHTML =
 document.getElementById("myHead").innerHTML =
 	"<div><span class=header><img src='/logo.png' class=header> KKB通販</span></div>"
 document.getElementById("myNav").innerHTML =
-	"<div style='height:35px; background-color: #FFFFCC; border: solid 3px #FFCC33; width:290px; display: inline-block;'><b><span style='font-size: 20px; vertical-align:middle'><a href='/oct.html'><center>23年2月1日起停收八達通</center></a></span></b></div>　<div style='height:35px; background-color: #FFFFCC; border: solid 3px #FFCC33; width:410px; display: inline-block;'><b><span style='font-size: 20px; vertical-align:middle'><a href='/done.html'><center>結業通知（落單、面交及郵寄、退款安排）</center></a></span></b></div>"
-+ "<ul id='navLinks'>"
+	 "<ul id='navLinks'>"
 	+ "<li><a href='/record/'>查單</a></li>"
 	+ "<li><a href='https://www.kkbee.net/depo' target='_blank'>入訂填表</a>"
 	+ "<li><a href='/yahooauc/'>日拍/販售App代購</a></li>"
